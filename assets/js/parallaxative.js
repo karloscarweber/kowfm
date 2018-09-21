f3b8c4e269645c688f835afb80521d87f2e21673
karloscarweber <me@kow.fm>
2018-09-21 11:37:09 -0600

    Improved performance on Chrome.

--- assets/js/parallaxative.js
+++ assets/js/parallaxative.js
@@ -7,27 +7,27 @@ var parallaxController;
 
 	parallaxController = this;
 
-  function extend(){
-      for(var i=1; i<arguments.length; i++)
-          for(var key in arguments[i])
-              if(arguments[i].hasOwnProperty(key))
-                  arguments[0][key] = arguments[i][key];
-      return arguments[0];
-  }
+	function extend(){
+		for(var i=1; i<arguments.length; i++)
+			for(var key in arguments[i])
+				if(arguments[i].hasOwnProperty(key))
+					arguments[0][key] = arguments[i][key];
+		return arguments[0];
+	}
 
 	// HTML Stuff
 	this.paralaxxed = document.getElementsByClassName("parallaxy-animate");
 
-  // Create the logic
-  parallaxate = function(parallax_elements, defaultSettings) {
-    var elements = parallax_elements;
+	// Create the logic
+	parallaxate = function(parallax_elements, defaultSettings) {
+		var elements = parallax_elements;
 
-    for (i = 0; i < elements.length; i++) {
+		for (i = 0; i < elements.length; i++) {
 			// console.log('running')
 
-      var settings = JSON.parse(JSON.stringify(defaultSettings)); // defaultSettings.copy();
-      var options = JSON.parse(elements[i].getAttribute('parallaxy-options'));
-      elements[i].settings = extend(settings, options);
+			var settings = JSON.parse(JSON.stringify(defaultSettings)); // defaultSettings.copy();
+			var options = JSON.parse(elements[i].getAttribute('parallaxy-options'));
+			elements[i].settings = extend(settings, options);
 
       if (options.positionType == 'relative') {
           elements[i].initial_offset = parseInt(window.getComputedStyle(elements[i], null).getPropertyValue('margin-top'), 10);
@@ -37,19 +37,18 @@ var parallaxController;
           elements[i].dataset.positionType = "absolute";
       }
 
-      elements[i].dataset.currentDelta = 0;
-      elements[i].dataset.newDelta = 0;
+			elements[i].dataset.currentDelta = 0;
+			elements[i].dataset.newDelta = 0;
     }
-  }
+	}
 
 	scrollHandler = function() {
 
 		var that = this;
 		var scrollTop = window.pageYOffset;
 
-    for (i = 0; i < paralaxxed.length; i++) {
-
-	    var currentDelta = paralaxxed[i].dataset.currentDelta;
+		for (i = 0; i < paralaxxed.length; i++) {
+			var currentDelta = paralaxxed[i].dataset.currentDelta;
 
 			var newDelta = (0 + (scrollTop * paralaxxed[i].settings.multiplier));
 			// var tweenDelta = (currentDelta - ((currentDelta - newDelta) * 0.08));
@@ -57,27 +56,27 @@ var parallaxController;
 			// var defaultRotation = " rotate(-45deg) ";
 			var defaultRotation = paralaxxed[i].settings.rotation;
 
-      paralaxxed[i].style.transform = "translateY(" + tweenDelta + "px) translateZ(0)" + defaultRotation;
-      paralaxxed[i].style.webkitTransform = "translateY(" + tweenDelta + "px) translateZ(0)" + defaultRotation;
-      paralaxxed[i].dataset.currentDelta = tweenDelta;
-    }
+			paralaxxed[i].style.transform = "translateY(" + tweenDelta + "px) translateZ(0)" + defaultRotation;
+			paralaxxed[i].style.webkitTransform = "translateY(" + tweenDelta + "px) translateZ(0)" + defaultRotation;
+			paralaxxed[i].dataset.currentDelta = tweenDelta;
+		}
 
 		// so scrollHandler calls itself to keep getting triggered when we paint.
-    // window.requestAnimationFrame( scrollHandler );
-  }
+		// window.requestAnimationFrame( scrollHandler );
+	}
 
   // init the thing
   function init() {
     var that = this;
     var paralaxxedsettings = {
-         "multiplier" : "0.2",
-         "positionType": "absolute",
-				 "rotation": "rotate(-45deg)",
+			"multiplier" : "0.2",
+			"positionType": "absolute",
+			"rotation": "rotate(-45deg)",
     };
 
-    if(paralaxxed.length > 0) {
-        parallaxate(that.paralaxxed, paralaxxedsettings);
-    }
+		if(paralaxxed.length > 0) {
+			parallaxate(that.paralaxxed, paralaxxedsettings);
+		}
 		console.log('parallaxy started')
 		scrollHandler();
   }
